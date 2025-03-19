dd252857d88df20c70f4c6424f454aecb76a1f05
epicmann24 <110631325+epicmann24@users.noreply.github.com>
2025-03-19 21:11:34 +0000

    Update and rename service.sh to post-fs-data.sh

--- revanced-magisk/post-fs-data.sh
+++ revanced-magisk/post-fs-data.sh
@@ -4,15 +4,9 @@
 
 app_name=$(APPNAME)
 BASEPATH="/product/app/$app_name"
-
-mkdir -p "$MODPATH/product/app/$app_name"
-
 RVPATH="$MODPATH/product/app/$app_name/$app_name.apk"
-
-mv -f "$MODPATH/base.apk" "$RVPATH"
 
 mm mount --bind "$RVPATH" "$BASEPATH/base.apk"
 
-am force-stop "$PKG_NAME"
 
 run
